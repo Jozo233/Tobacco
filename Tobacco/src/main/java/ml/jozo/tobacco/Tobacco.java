471ea85e1cd4e6c5131ec08850aa67af9ed3e614
Jozo_85 <67067963+Jozo233@users.noreply.github.com>
2024-06-15 21:20:20 +0200

    Fix

--- Tobacco/src/main/java/ml/jozo/tobacco/Tobacco.java
+++ Tobacco/src/main/java/ml/jozo/tobacco/Tobacco.java
@@ -1,11 +1,10 @@
 package ml.jozo.tobacco;
 
 import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
-import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
-import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
+
 import io.github.thebusybiscuit.slimefun4.libraries.unirest.Config;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
+import ml.jozo.tobacco.items.WildTobacco;
+import ml.jozo.tobacco.items.Tobacco
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Tobacco extends JavaPlugin implements SlimefunAddon {
@@ -20,10 +19,10 @@ public class Tobacco extends JavaPlugin implements SlimefunAddon {
         // Plugin startup logic
         Config cfg = new Config(this);
 
-        NamespacedKey categoryId = new NamespacedKey(this, "tobacco");
-        CustomItemStack stackId = new CustomItemStack(Material.ACACIA_LEAVES, "&4Tobacco Plans and thing from it");
+        new WildTobacco(this);
 
-        ItemGroup itemGroup = new ItemGroup(categoryId, stackId);
+
+        new ml.jozo.tobacco.items.Tobacco(this);
 
     }
 
--- Tobacco/src/main/java/ml/jozo/tobacco/items/WildTobacco.java
+++ Tobacco/src/main/java/ml/jozo/tobacco/items/WildTobacco.java
@@ -1,28 +1,17 @@
 package ml.jozo.tobacco.items;
 
-import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
-import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
-import jdk.tools.jlink.plugin.Plugin;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 
-public class WildTobacco {
+public class WildTobacco extends SlimefunItem {
 
-    public static void register(Plugin plugin){
-        NamespacedKey categoryId = new NamespacedKey(this, "tobacco");
-        CustomItemStack stackId = new CustomItemStack(Material.ACACIA_LEAVES, "&4Tobacco Plans and thing from it");
-
-        ItemGroup itemGroup = new ItemGroup(categoryId, stackId);
-
-        SlimefunItemStack itemStack = new  SlimefunItemStack("WILD_TOBBCO", Material.ALLIUM, "Wild Tobacco",  "&8A rare herb found in grass");
-        ItemStack[] recipe = {};
-        SlimefunItem sfItem = new SlimefunItem(itemGroup, itemStack, RecipeType.NULL, recipe);
-        sfItem.register(plugin);
-
-
+    public WildTobacco(SlimefunAddon addon){
+        super(addon, new SlimefunItemStack("WILD_TOBACCO", Material.ALLIUM, "&6Wild Tobacco"), RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[] {null, null, null, null, new ItemStack(Material.SHEARS), null, null, new SlimefunItemStack("TOBACCO", Material.CORNFLOWER), null});
+        register(addon);
     }
 }
